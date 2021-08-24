7614883529cdb8eca0d899fdf7ade6f751838edd
amolu <amolujagare@gmail.com>
2021-08-24 19:13:39 +0530

    ui Testing

--- testsrc/UITesting/LoginTest.java
+++ testsrc/UITesting/LoginTest.java
@@ -1,10 +1,13 @@
 package UITesting;
 
+import org.openqa.selenium.support.Color;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.Login;
 import util.OpenUrl;
+
+import java.util.Locale;
 
 public class LoginTest extends OpenUrl {
 
@@ -96,6 +99,92 @@ public class LoginTest extends OpenUrl {
         System.out.println("Actual="+actual);
 
         Assert.assertEquals(actual,expected,"incorrect font size");
+
+    }
+    @Test
+    public void lblEmailFontFamily()
+    {
+        String expected  ="-apple-system, system-ui, BlinkMacSystemFont, \"Segoe UI\", Roboto, \"Helvetica Neue\", Arial, sans-serif";
+
+        String actual = "";
+        try
+        {
+            actual = login.lblEmail.getCssValue("font-family");
+        }
+        catch (Exception e)
+        {
+
+        }
+
+        System.out.println("Expected="+expected);
+        System.out.println("Actual="+actual);
+
+        Assert.assertEquals(actual,expected,"incorrect font ");
+
+
+    }
+
+    @Test
+    public void lblEmailFont()
+    {
+        String expected  = "Times New Roman";
+
+        String actual = "";
+        try
+        {
+            actual = login.lblEmail.getCssValue("font-family");
+        }
+        catch (Exception e)
+        {
+
+        }
+
+        boolean result = actual.contains(expected);
+
+        Assert.assertTrue(result,"given font is not in the font family");
+    }
+
+    @Test
+    public void txtEmailWatermark()
+    {
+        String expected = "Email";
+        String actual = "";
+        try
+        {
+            actual = login.txtUser.getAttribute("placeholder");
+        }
+        catch (Exception e)
+        {
+        }
+
+        System.out.println("Expected="+expected);
+        System.out.println("Actual="+actual);
+
+        Assert.assertEquals(actual,expected,"incorrect value ");
+
+    }
+
+    @Test
+    public void btnColorCheck()
+    {
+        String expected = "#2C8EDD";
+        String actual = "";
+        try
+        {
+            String rgbColor = login.btnLogin.getCssValue("background-color");
+
+            actual = Color.fromString(rgbColor).asHex().toUpperCase(Locale.ROOT);
+        }
+        catch (Exception e)
+        {
+
+        }
+
+        System.out.println("Expected="+expected);
+        System.out.println("Actual="+actual);
+
+        Assert.assertEquals(actual,expected,"incorrect color");
+
 
     }
 }
